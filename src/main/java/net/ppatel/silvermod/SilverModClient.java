48e1832f33cabb2b9e84f3ba3c060b96e35afc65
Forestry3 <111252230+Protector9@users.noreply.github.com>
2022-12-02 22:36:50 -0500

    Initial commit

---
+++ src/main/java/net/ppatel/silvermod/SilverModClient.java
@@ -0,0 +1,10 @@
+package net.ppatel.silvermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SilverModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
